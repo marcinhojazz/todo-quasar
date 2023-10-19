c704925153890ae7b8574126618177f90d0fcdfc
marcinhojazz <marcinhojazz@gmail.com>
2023-10-19 17:23:28 -0300

    remove unused items

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,10 +1,20 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+  <q-page class="flex h-screen flex-center">
+    <div class="space-y-2 px-64 w-full h-full text-center text-2xl text-white bg-gray-800">
+      <h2>Welcome Todo List Quasar</h2>
+      <div class="bg-slate-200 flex p-6">
+        <p>1 - </p>
+        <p>Task 1</p>
+      </div>
+      <div class="bg-slate-200 flex p-6">
+        <p>1 - </p>
+        <p>Task 1</p>
+      </div>
+      <div class="bg-slate-200 flex p-6">
+        <p>1 - </p>
+        <p>Task 1</p>
+      </div>
+    </div>
   </q-page>
 </template>
 
