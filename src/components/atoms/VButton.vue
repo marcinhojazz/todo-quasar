eb4d1df8d54f24997d1ca5c2b7133e316dc64662
marcinhojazz <marcinhojazz@gmail.com>
2023-10-20 18:17:14 -0300

    renamed vbutton for vuepure

---
+++ src/components/atoms/VButton.vue
@@ -0,0 +1,56 @@
+<template>
+  <button :class="buttonClass" @click="handleClick">
+    {{ label }}
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    label: String,
+    buttonColor: String // Nova prop para especificar a cor do botão
+  },
+  setup (props, { emit }) {
+    const buttonClass = `custom-button ${props.buttonColor || 'default'}`
+
+    const handleClick = () => {
+      emit('click')
+    }
+
+    return {
+      buttonClass,
+      handleClick
+    }
+  }
+}
+</script>
+
+<style scoped>
+.custom-button {
+  padding: 10px 20px;
+  cursor: pointer;
+  border-radius: 4px;
+}
+
+.default {
+  background-color: #ccc;
+  color: #333;
+}
+
+.primary {
+  background-color: #007bff;
+  color: #fff;
+}
+.secondary {
+  background-color: #26A69A;
+  color: #fff;
+}
+.amber {
+  background-color: #F2C037;
+  color: #fff;
+}
+.warning {
+  background-color: #f23737;
+  color: #fff;
+}
+</style>
