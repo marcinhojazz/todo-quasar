4ce2794bf4be220eb893c0b6ba5e6b0ffc558e34
marcinhojazz <marcinhojazz@gmail.com>
2023-10-20 10:42:14 -0300

    create simple input component

---
+++ src/components/atoms/MyInput.vue
@@ -0,0 +1,22 @@
+<template>
+  <q-input
+    v-model="inputValue"
+    @input="$emit('input', inputValue)"
+    :placeholder="placeholder"
+  />
+</template>
+
+<script>
+export default {
+  name: 'MyInput',
+  props: {
+    value: String,
+    placeholder: String
+  },
+  data () {
+    return {
+      inputValue: this.value
+    }
+  }
+}
+</script>
