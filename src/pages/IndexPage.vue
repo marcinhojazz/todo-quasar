e1b12ac0d3601b058636867eafd2a4133fb38670
marcinhojazz <marcinhojazz@gmail.com>
2023-10-20 10:43:25 -0300

    created components on the view

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,18 +1,11 @@
 <template>
   <q-page class="flex h-screen flex-center">
-    <div class="space-y-2 px-64 w-full h-full text-center text-2xl text-white bg-gray-800">
+    <div class="space-y-2 px-64 w-full h-full text-center text-2xl text-black bg-gray-200">
       <h2>Welcome Todo List Quasar</h2>
-      <div class="bg-slate-200 flex p-6">
-        <p>1 - </p>
-        <p>Task 1</p>
-      </div>
-      <div class="bg-slate-200 flex p-6">
-        <p>1 - </p>
-        <p>Task 1</p>
-      </div>
-      <div class="bg-slate-200 flex p-6">
-        <p>1 - </p>
-        <p>Task 1</p>
+      <div>
+        <MyButton label="Click Aqui"/>
+        <MyInput v-model="inputValue" placeholder="Digite algo" />
+        <MyCheckbox v-model="isChecked" label="Marque esta opção" />
       </div>
     </div>
   </q-page>
@@ -20,8 +13,22 @@
 
 <script>
 import { defineComponent } from 'vue'
+import MyButton from '../components/atoms/MyButton.vue'
+import MyInput from '../components/atoms/MyInput.vue'
+import MyCheckbox from '../components/atoms/MyCheckbox.vue'
 
 export default defineComponent({
-  name: 'IndexPage'
+  name: 'IndexPage',
+  components: {
+    MyButton,
+    MyInput,
+    MyCheckbox
+  },
+  data () {
+    return {
+      inputValue: ''
+    }
+  }
+
 })
 </script>
