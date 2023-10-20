aa8c405e456561b0a6ac8518c95a139634c019e8
marcinhojazz <marcinhojazz@gmail.com>
2023-10-20 10:42:37 -0300

    create simple mycheckbox component

---
+++ src/components/atoms/MyCheckbox.vue
@@ -0,0 +1,21 @@
+<template>
+  <label>
+    <q-checkbox v-model="isChecked" @input="$emit('input', isChecked)" class="text-primary" />
+    <span>{{ label }}</span>
+  </label>
+</template>
+
+<script>
+export default {
+  name: 'MyCheckbox',
+  props: {
+    value: Boolean,
+    label: String
+  },
+  data () {
+    return {
+      isChecked: this.value || false
+    }
+  }
+}
+</script>
