f892f6695bf0c423ca230d401be8ff0c442c6f17
marcinhojazz <marcinhojazz@gmail.com>
2023-10-20 10:42:04 -0300

    create simple mybutton component

---
+++ src/components/atoms/MyButton.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-btn
+    color="primary"
+    @click="$emit('click')"
+  >
+    {{ label }}
+  </q-btn>
+</template>
+
+<script>
+export default {
+  name: 'MyButton',
+  props: {
+    label: String
+  }
+}
+</script>
